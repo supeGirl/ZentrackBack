1251deb7944a8e2edcc6eb0dbe475d9ec0e03175
supaGirl <kerenvasserman.bm@gmail.com>
2025-05-07 19:15:18 +0300

    Update user service

--- middlewares/requireAuth.middleware.js
+++ middlewares/requireAuth.middleware.js
@@ -1,36 +1,40 @@
-import { userService } from "../api/user/user.service.js";
+import {userService} from '../api/user/user.service.js'
+import {asyncLocalStorage} from '../services/als.service.js'
 
 export function requireAuth(req, res, next) {
-  const { loggedinUser } = asyncLocalStorage.getStore();
-  req.loggedinUser = loggedinUser;
+  const {loggedinUser} = asyncLocalStorage.getStore()
 
-  if (config.isGuestMode && !loggedinUser) {
-    req.loggedinUser = { _id: '', fullname: 'Guest' };
-    return next();
-  }
+  req.loggedinUser = loggedinUser
 
-  if (!loggedinUser) return res.status(401).send('Not Authenticated');
+  // if (config.isGuestMode && !loggedinUser) {
+  //   req.loggedinUser = { _id: '', fullname: 'Guest' };
+  //   return next();
+  // }
 
-  userService.getById(loggedinUser.id)
-    .then(user => {
-      req.loggedinUser = user;
-      next();
+  if (!loggedinUser) return res.status(401).send('Not Authenticated')
+
+  userService
+    .getById(loggedinUser._id)
+    .then((user) => {
+      req.loggedinUser = user
+      next()
     })
-    .catch(err => res.status(401).send('User not found in local data'));
+    .catch((err) => res.status(401).send('User not found in local data'))
 }
 
 export function requireAdmin(req, res, next) {
-  const { loggedinUser } = asyncLocalStorage.getStore();
+  const {loggedinUser} = asyncLocalStorage.getStore()
 
-  if (!loggedinUser) return res.status(401).send('Not Authenticated');
+  if (!loggedinUser) return res.status(401).send('Not Authenticated')
 
-  userService.getById(loggedinUser.id)
-    .then(user => {
+  userService
+    .getById(loggedinUser._id)
+    .then((user) => {
       if (!user.isAdmin) {
-        logger.warn(`${loggedinUser.fullname} attempted to perform an admin action`);
-        return res.status(403).send('Not Authorized');
+        logger.warn(`${loggedinUser.fullname} attempted to perform an admin action`)
+        return res.status(403).send('Not Authorized')
       }
-      next();
+      next()
     })
-    .catch(err => res.status(401).send('User not found in local data'));
+    .catch((err) => res.status(401).send('User not found in local data'))
 }
